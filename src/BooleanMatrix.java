f1e5002a04490419aac6886e17eb76dd8f30bf6b
Anupam Manoj <anupammanoj1157@gmail.com>
2026-04-05 22:17:39 +0530

    Completed boolMatrix

--- src/BooleanMatrix.java
+++ src/BooleanMatrix.java
@@ -1,14 +1,37 @@
-public class BooleanMatrix {
-    
-    // TODO: Implement the function to modify the matrix
-    
-        // Logic: Check each row. If it contains a 1, set the whole row to 1.
-    
+import java.util.Scanner;
 
-    
-        // TODO: Read m and n
-        // TODO: Read the matrix elements
-        // TODO: Call BooleanMatrixProblem(mat, m, n)
-        // TODO: Print the modified matrix
-    
+public class BooleanMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read dimensions
+        if (!sc.hasNextInt())
+            return;
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            boolean hasOne = false;
+            int[] row = new int[n];
+
+            // Read the current row and check for any '1'
+            for (int j = 0; j < n; j++) {
+                row[j] = sc.nextInt();
+                if (row[j] == 1) {
+                    hasOne = true;
+                }
+            }
+
+            // Print the output for this row based on the flag
+            for (int j = 0; j < n; j++) {
+                if (hasOne) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+        sc.close();
+    }
 }
